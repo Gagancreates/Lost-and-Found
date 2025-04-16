fdb58524309e894c821f7e29602363e3c402e449
Gagancreates <gaganp000999@gmail.com>
2025-04-16 19:43:59 +0530

    place holder value fixed now moving on to integrate with backend

--- components/post-form.tsx
+++ components/post-form.tsx
@@ -139,7 +139,7 @@ export default function PostForm({ type, onSubmit, onCancel }: PostFormProps) {
           onChange={handleChange}
           required
           className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400"
-          placeholder="Your name and roll number"
+          placeholder="How to contact you?( Email, Phone Number, etc.)"
         />
       </div>
 
